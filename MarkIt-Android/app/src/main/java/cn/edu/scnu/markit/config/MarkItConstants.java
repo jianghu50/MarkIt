225936c312f5acb12f1ea99ace2a0cbc3352d08e
Vernon <1395872519@qq.com>
2016-05-12 10:08:23 +0800

    Revert "Dev"

--- MarkIt-Android/app/src/main/java/cn/edu/scnu/markit/config/MarkItConstants.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.scnu.markit.config;
-
-/**
- * Created by jialin on 2016/4/28.
- */
-public class MarkItConstants {
-    public static String NET_WORK_ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
-}
--- MarkIt/MainWindow.xaml.cs
+++ MarkIt/MainWindow.xaml.cs
@@ -10,23 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MarkIt.SignInAndSignUp.View
+namespace MarkIt
 {
     /// <summary>
-    /// SignUpWindow.xaml 的交互逻辑
+    /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class SignUpWindow : Window
+    public partial class MainWindow : Window
     {
-        public SignUpWindow()
+        public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void backButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
