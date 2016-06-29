499d3164bf2678e8ab20b5f53157dd563dcde7a0
KasperLiu <elysioncs13b@163.com>
2016-06-29 08:39:30 +0800

    完成了ChangeLog的数据结构;同步功能，只剩下插入Android本地数据库的语句未完成

--- MarkIt-Android/app/src/main/java/cn/edu/scnu/markit/config/MarkItConstants.java
+++ MarkIt-Android/app/src/main/java/cn/edu/scnu/markit/config/MarkItConstants.java
@@ -6,6 +6,12 @@ import android.view.ViewGroup;
  * Created by jialin on 2016/4/28.
  */
 public class MarkItConstants {
-    public static String NET_WORK_ACTION = "an droid.net.conn.CONNECTIVITY_CHANGE";
+    public static String NET_WORK_ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
     public static final int ROW_HEIGHT = ViewGroup.LayoutParams.WRAP_CONTENT;
+    //同步中使用的常量
+    public static final String CHANGELOG_TYPE_CONTACT = "CONTACT";
+    public static final String CHANGELOG_TYEP_NOTE = "NOTE";
+    public static final String CHANGELOG_OPERATION_CREATE = "CREATE";
+    public static final String CHANGELOG_OPERATION_EDIT = "EDIT";
+    public static final String CHANGELOG_OPERATION_DELETE = "DELETE";
 }
